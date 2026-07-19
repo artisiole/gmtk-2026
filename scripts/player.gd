af2334d8e691aa55112b9665e7064cf7265952fc
artisiole <43116512+artisiole@users.noreply.github.com>
2026-07-19 16:15:45 -0500

    add player sprint

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,15 @@ extends Sprite2D
 # Extremely rudimentary top down movement script
 # Will be replaced
 
-const SPEED = 500
+var SPEED = 400
 
 func _process(delta: float) -> void:
 	var input = Input.get_vector("left", "right", "up", "down").normalized()
 	
 	global_position.x += input.x * delta * SPEED
 	global_position.y += input.y * delta * SPEED
+	
+	if Input.is_action_pressed("sprint"):
+		SPEED = 700
+	else:
+		SPEED = 400
