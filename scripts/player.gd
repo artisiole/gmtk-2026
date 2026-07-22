bab7e87ad036ebca6d70c5928c0d5eb6d3164457
artisiole <43116512+artisiole@users.noreply.github.com>
2026-07-22 12:22:56 -0500

    basic characterbody2d movement and collision

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,18 @@
-extends Sprite2D
+extends CharacterBody2D
 
 # Extremely rudimentary top down movement script
 # Will be replaced
 
 var SPEED = 400
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var input = Input.get_vector("left", "right", "up", "down").normalized()
 	
-	global_position.x += input.x * delta * SPEED
-	global_position.y += input.y * delta * SPEED
-	
 	if Input.is_action_pressed("sprint"):
-		SPEED = 700
-	else:
 		SPEED = 400
+	else:
+		SPEED = 300
+	
+	velocity = input * SPEED
+	
+	move_and_slide()
