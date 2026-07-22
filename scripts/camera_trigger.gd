b1bc4c08e23bc800e1743fa2ed22007b6ef3676e
artisiole <43116512+artisiole@users.noreply.github.com>
2026-07-22 18:57:07 -0500

    layout in main scene

---
+++ scripts/camera_trigger.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@export var camera: Camera2D
+@export var destination: Vector2
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		camera.global_position = destination
