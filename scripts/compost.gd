12e87bcbcd12088688bbc4755b13b442c66429ac
artisiole <43116512+artisiole@users.noreply.github.com>
2026-07-23 14:42:07 -0500

    pot planting functionality

--- scripts/compost.gd
+++ scripts/compost.gd
@@ -9,3 +9,6 @@ func _on_body_entered(body: Node2D) -> void:
 		plot.plantTime = 0.0
 		plot.plant_sprite.texture = null
 		plot.get_node("PlantInteract").position = Vector2.ZERO
+	if body.is_in_group("potted_plant"):
+		body.end_interact()
+		body.get_node("PotPlantSprite").texture = null
--- scripts/pot.gd
+++ scripts/pot.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
+	print("entered")
 	if body.is_in_group("plant"):
 		# Same as compost, except set a new plant
+		
 		body.end_interact()
 		var plot = body.get_parent()
+		get_node("PlantInteract/PotPlantSprite").texture = plot.plant_sprite.texture
+		
 		plot.planted = false
 		plot.plant_plucked = false
 		plot.plantTime = 0.0
