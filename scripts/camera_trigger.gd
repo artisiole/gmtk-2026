5f84f24057377e888e12bd744ec0e7a488adf184
artisiole <43116512+artisiole@users.noreply.github.com>
2026-07-23 13:27:04 -0500

    basic pot functionality + other polish

--- scripts/camera_trigger.gd
+++ scripts/camera_trigger.gd
@@ -2,8 +2,16 @@ extends Area2D
 
 @export var camera: Camera2D
 @export var destination: Vector2
+@export var pots: Node2D
+@export var pot_dest: Node2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		camera.global_position = destination
 		body.camera_y_pos = destination.y
+		
+		pots.global_position = pot_dest.global_position
+		
+		# fix bug with pot plant not moving properly
+		for pot in pots.get_children():
+			pot.get_node("PlantInteract").position.y = -18
--- scripts/potted_plant.gd
+++ scripts/potted_plant.gd
@@ -3,14 +3,18 @@ extends StaticBody2D
 var interacting: bool = false
 var current_interactor
 
+@onready var pot = get_parent()
+
 func start_interact(interactor):
 	interacting = true
 	current_interactor = interactor
 
 func end_interact():
 	interacting = false
+	position = Vector2(0, -18)
 
 func _process(delta: float) -> void:
+	print(position)
 	# interact behavior
 	if interacting:
 		set_collision_layer_value(3, false)
