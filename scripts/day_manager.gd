3b6439cba7e4d6d0b0a253c2a19637b2206e1aaa
Bee! <thesquafe@gmail.com>
2026-07-23 05:12:34 -0500

    endofnight1
    everything i needed to do i did (as well as i could) and i played around with new things as well
    
    also FONT !!!! YEAH !!!!

--- scripts/day_manager.gd
+++ scripts/day_manager.gd
@@ -3,10 +3,16 @@ extends Node2D
 @export var camera: Camera2D
 @export var destination: Vector2
 
-var time_remaining: float = 7.0
+var time_remaining: float = 301.0
 var timing: bool = true
+var day: bool = true
+var evening: bool = false
+var night: bool = false
 
 signal timeout
+signal daytime
+signal sunset
+signal nighttime
 
 @onready var timerlabel = $Control/TimerLabel
 
@@ -24,15 +30,39 @@ func _process(delta: float) -> void:
 	else:
 		timerlabel.text = " " + str(minutes) + ":" + str(int(seconds))
 	
+	# Next few if statements are to send signals so I can trigger time of day effects.
+	
+	if minutes > 2 and night == true:
+		daytime.emit()
+		day = true
+		night = false
+		# a really jank placeholder way for me to send a reset signal to set it back to day
+	
+	if minutes < 2 and day == true:
+		sunset.emit()
+		day = false
+		evening = true
+	
+	if minutes < 1 and evening == true:
+		nighttime.emit()
+		evening = false
+		night = true
 	
 	if time_remaining < 0 and timing == true:
 		timeout.emit()
 		var tween = get_tree().create_tween()
 		tween.tween_property($"..", "global_position", Vector2(640.0, -360.0), 2.5).set_trans(Tween.TRANS_SPRING)
 		timing = false
-	# Because we're setting timing to be false here, this statement can only be true for one frame.
+	
+	# Because we're setting timing to be false here, these statements can only be true for one frame.
 	# So (hopefully smartly and safely) it's called for until it's true and then STOPS.
-	# Therefore, only on the frame [singular] that this is true does it call for the camera motion and send a signal.
+	# Therefore, only on the frame [singular] that this is true does it send a signal.
+	# This attaches to SkyOverlay and a script I'd love to run on the lights.
+	
+	# ^^ ON THE LIGHTS ^^
+	# I have ONE light working with the signals. For some reason if they all have the same script it's still only the
+	# first one that turns on or off with the signals. Unsure what that is but it's almost 5 and I gotta make sure everything else still works.
+	# If you wanna figure it out while I'm sleeping, that'd be huge. "light.gd" exists and is attached to one of the lights. Go ham.
 	
 	if timing == false:
 		timerlabel.text = " "
---
+++ scripts/sky_overlay.gd
@@ -0,0 +1,36 @@
+extends Sprite2D
+
+@onready var sky_overlay: Sprite2D = $"."
+
+# On the "sky_overlay.modulates" :
+# These just tween to the next color unless it's the reset, in which case it just SNAPS to white.
+
+func _on_day_manager_daytime():
+	sky_overlay.modulate = Color("White")
+	
+func _on_day_manager_sunset():
+	var modulate_color : Color
+	modulate_color.r = 1.02
+	modulate_color.g = 0.719
+	modulate_color.b = 0.611
+	modulate_color.a = 255
+	#sky_overlay.modulate = Color(1.02, 0.719, 0.611)
+	
+	# I don't think you HAVE to set "modulate_color" to mean something per value here, but it's done.
+	# Thank you random Reddit problem solvers. :/
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property($".", "modulate", modulate_color, 7.5).set_trans(Tween.TRANS_SINE)
+	
+	
+func _on_day_manager_nighttime():
+	var modulate_color : Color
+	modulate_color.r = 0.565
+	modulate_color.g = 0.555
+	modulate_color.b = 1.235
+	modulate_color.a = 255
+	#sky_overlay.modulate = Color(0.565, 0.555, 1.235)
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property($".", "modulate", modulate_color, 7.5).set_trans(Tween.TRANS_SINE)
+	
