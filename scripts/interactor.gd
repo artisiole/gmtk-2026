90165defd82b21a73a8e5b5572ef6417f2cd1bf5
artisiole <43116512+artisiole@users.noreply.github.com>
2026-07-23 09:46:43 -0500

    seed bugs should be fixed now

--- scripts/interactor.gd
+++ scripts/interactor.gd
@@ -7,17 +7,21 @@ func _process(delta: float) -> void:
 		
 		# Dodgy
 		# If just pressed, prioritize seeds
-		if get_overlapping_bodies().size() > 1 and get_overlapping_bodies()[1].is_in_group("seed"):
-			get_overlapping_bodies()[1].start_interact(self)
-		else:
-			# Every single interactable should have a function called start_interact and end_interact
-			# This is a little dodgy but who cares
-			get_overlapping_bodies()[0].start_interact(self)
-		
-	if Input.is_action_just_released("interact") and has_overlapping_bodies():
+		#if get_overlapping_bodies().size() > 1 and get_overlapping_bodies()[1].is_in_group("seed"):
+			#get_overlapping_bodies()[1].start_interact(self)
+		#else:
 		# Every single interactable should have a function called start_interact and end_interact
 		# This is a little dodgy but who cares
-		get_overlapping_bodies()[0].end_interact()
+		get_overlapping_bodies()[0].start_interact(self)
+		
+	if not Input.is_action_pressed("interact") and has_overlapping_bodies(): #Input.is_action_just_released("interact")
+		# Every single interactable should have a function called start_interact and end_interact
+		# This is a little dodgy but who cares
+		
+		# When releasing, we should go through every overlapping body and release it
+		# This is to prevent buggy behavior with seeds overlapping plots
+		for body in get_overlapping_bodies():
+			body.end_interact()
 	
 	if has_overlapping_bodies():
 		if get_overlapping_bodies().size() > 1 and get_overlapping_bodies()[0].is_in_group("seed"):
@@ -26,4 +30,3 @@ func _process(delta: float) -> void:
 			InteractionTracker.interactable_object = get_overlapping_bodies()[1]
 		else:
 			InteractionTracker.interactable_object = get_overlapping_bodies()[0]
-		
